c19018355d847aa9867ad27d9a2f8e02e50ac45c
Areeb Arshad <areebarshad.m@gmail.com>
2024-07-19 14:34:36 +0500

    initial commit - project setup

---
+++ src/components/buttons/button.component.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { IButtonProps } from '../../interfaces/button.interface';
+
+export default function Button({
+  title,
+  className,
+}: IButtonProps): React.ReactElement {
+  return (
+    <button
+      className={`py-2 px-4 m-1 bg-emerald-800 text-white text-sm rounded-md ${className}`}
+    >
+      {title}
+    </button>
+  );
+}
---
+++ src/components/forms/form.component.tsx
@@ -0,0 +1,9 @@
+import Button from '../buttons/button.component';
+
+export default function Form() {
+  return (
+    <div>
+      <Button title="Submit" />
+    </div>
+  );
+}
