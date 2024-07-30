326ea29f7ee2dbdfe3645669e0cabf44c5abd60f
Araib khan <araibkhan98@gmail.com>
2024-07-30 17:27:02 +0500

    formik form with navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
 import './App.css';
-// import { BrowserRouter, Routes, Route } from 'react-router-dom';
-// import ControlledForm from './pages/controlled-form.page';
-// import UnControlledForm from './pages/Uncontrolled-form.page';
-// import Navbar from './components/navbar/navbar.component';
-// import Home from './pages/home.page';
-// import Formikform from './pages/formik-form.page';
-import UncontrolledFormikForm from './pages/formik-uncontrolled-form.page';
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import ControlledForm from './pages/controlled-form.page';
+import Navbar from './components/navbar/navbar.component';
+import Home from './pages/home.page';
+import FormikForm from './pages/formik-form.page';
+import UncontrolledFormikForm from './pages/formik-uncontrolled.page';
+import UnControlledForm from './pages/Uncontrolled-form.page';
 
 function App() {
   return (
     <>
-      {/* <BrowserRouter>
+      <BrowserRouter>
       <Navbar />
         <Routes>
           <Route path="/controlled-form" element={<ControlledForm />} />
-          <Route path="/uncontrolled-form" element={<UnControlledForm />} />
+          <Route path="/Uncontrolled-form" element={<UnControlledForm />} />
+          <Route path="/formik-form" element={<FormikForm />} />
+          <Route path="/formik-uncontrolled" element={<UncontrolledFormikForm/>} />
           <Route path="/" element={<Home />} />
         </Routes>
-      </BrowserRouter> */}
-      {/* <Formikform/> */}
-      <UncontrolledFormikForm/>
+      </BrowserRouter>
+     
 
 
     </>
